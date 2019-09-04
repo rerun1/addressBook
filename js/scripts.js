d670adb7d11c5330d50f830acd779aa047f0a31b
Suzi Rubino <suzi@suzirubi.com>
2019-09-03 22:08:39 -0700

    add script for deleteContact, findContact, updateContact

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,58 @@
 //Business Logic for AddressBook
 function AddressBook(){
   this.contacts = [];
+  this.currentID = 0;
 }
 AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
   this.contacts.push(contact);
 }
+AddressBook.prototype.assignId = function(){
+  this.currentID += 1;
+  return this.currentID;
+}
 
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+AddressBook.prototype.updateContactPhone = function(id, string) {
+  for (var i=0; i < this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        this.contacts[i].phoneNumber = string;
+        return this.contacts[i].phoneNumber;
+      }
+    }
+  };
+  return false;
+}
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i < this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i = 0; i < this.contacts.length; i++) {
+    if (this.contacts[i].id == id) {
+      return this.contacts[i];
+    }
+  }
+}
 //Business Logic for Contacts
 function Contact (firstName, lastName, phoneNumber) {
   this.firstName = firstName;
@@ -15,8 +62,6 @@ function Contact (firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function(){
   return this.firstName + " " + this.lastName;
 }
-
-
 
 //
 // var tomatoes = {name: "Tomatoes", price: 2.99};
